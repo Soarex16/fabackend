993e6c108afd279df19d613f66262c3b747d4e7c
Soarex16 <soarex16@gmail.com>
2020-04-01 01:37:27 +0300

    Gracefull shutdown

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/sirupsen/logrus"
 	"github.com/soarex16/fabackend/app"
@@ -15,19 +18,52 @@ var serveCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		application, err := app.New()
 
+		defer func() {
+			logrus.Info("Closing database connection")
+			application.Context.DB.Close()
+		}()
+
 		if err != nil {
 			logrus.Fatalf("Unable to start application: %v", err)
 		}
 
-		defer application.Context.DB.Close()
+		srv := newServer(application)
+		// start server in it's own goroutine
+		go func() {
+			if err = srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				logrus.Fatal(err)
+			}
+		}()
+		logrus.Infof("Server has been started ad port %v", application.Config.Port)
 
-		addr := fmt.Sprintf(":%v", application.Config.Port)
-		logrus.Infof("Starting server at port %v", addr)
+		// subscribe to interrupt
+		osSig := make(chan os.Signal, 1)
+		signal.Notify(osSig, os.Interrupt)
 
-		logrus.Fatal(http.ListenAndServe(addr, application.Router))
+		<-osSig
+		logrus.Info("Caught Interrupt signal. Shutting down")
 
-		return nil
+		// context for gracefull shutdown
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		if err = srv.Shutdown(ctx); err != nil {
+			err = nil
+			logrus.Errorf("Server unsuccessfully shutted down with error: %v", err)
+		}
+
+		return err
 	},
+}
+
+func newServer(app *app.App) *http.Server {
+	addr := fmt.Sprintf(":%v", (*app).Config.Port)
+	server := http.Server{
+		Addr:    addr,
+		Handler: (*app).Router,
+	}
+
+	return &server
 }
 
 //TODO: метод, который осуществляет корректное завершение работы приложения (и закрывает коннект к бд)
