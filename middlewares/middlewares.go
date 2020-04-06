3a68e701f43acfa5b7d3eca906486e070fadccff
Soarex16 <soarex16@gmail.com>
2020-04-06 13:42:42 +0300

    Attempt to fix CORS middleware

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -135,8 +135,13 @@ func Authorization(next http.Handler, store *auth.SessionStore) http.Handler {
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+
+		// Stop here if its Preflighted OPTIONS request
+		if r.Method == "OPTIONS" {
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
